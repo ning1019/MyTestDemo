b04b33c2917cf7b9eb8a7cca8102be43eec04a88
ning <>
2024-10-20 16:26:19 +0800

    新增按钮

--- MyTestDemo/ViewController.swift
+++ MyTestDemo/ViewController.swift
@@ -16,6 +16,15 @@ class ViewController: UIViewController {
         print("First commit")
         print("测试第一次上传代码成功了吗")
         self.view.backgroundColor = .red
+        
+        let btn = UIButton()
+        btn.frame = CGRect(x: 80, y: 80, width: 200, height: 50)
+        btn.setTitle("测试", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = .brown
+        self.view.addSubview(btn)
+        btn.layer.cornerRadius = 8
+        btn.layer.masksToBounds = true
     }
 
 
