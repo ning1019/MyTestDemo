ae30791adddee15d751d85e6fa64cad67d57a9e3
ning <>
2024-10-20 16:14:10 +0800

    改变主页背景色

--- MyTestDemo/ViewController.swift
+++ MyTestDemo/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("First commit")
+        print("测试第一次上传代码成功了吗")
+        self.view.backgroundColor = .red
     }
 
 
