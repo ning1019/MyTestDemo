63babe5b1388f68995583f765027769ce544bb4b
ning <>
2024-10-20 15:57:51 +0800

    First commit

---
+++ MyTestDemoTests/MyTestDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestDemoTests.swift
+//  MyTestDemoTests
+//
+//  Created by ning on 2024/10/20.
+//
+
+import Testing
+@testable import MyTestDemo
+
+struct MyTestDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
